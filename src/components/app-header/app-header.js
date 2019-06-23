8e4032f8aef1911fdb9ac6821555dc7dcd9de907
nikita-parkhomenko <nikita.parkhom@gmail.com>
2019-06-23 11:47:27 +0300

    Final version of app React structure

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './app-header.css';
 
 const AppHeader = ({toDo, done}) => {
---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Go to Tinder Date', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Play tennis', important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+export default App;
